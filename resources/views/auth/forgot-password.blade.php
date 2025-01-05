76a024491a0691b4c7b4a6c20a7526ba733bfa8e
Md. Arif Hassan <aarifhsn@gmail.com>
2025-01-05 10:24:14 +0600

    removed manual email verification options

--- resources/views/auth/forgot-password.blade.php
+++
@@ -1,6 +0,0 @@
-<form method="POST" action="{{ route('password.email') }}">
-    @csrf
-    <label>Email</label>
-    <input type="email" name="email" required>
-    <button type="submit">Send Reset Link</button>
-</form>
